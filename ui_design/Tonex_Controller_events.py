1bf3c109c72df5c64c6338afc14456fb784f9deb
Builty <greg.s@zoneempire.com>
2024-10-29 16:36:41 +1100

    Add files via upload

---
+++ ui_design/Tonex_Controller_events.py
@@ -0,0 +1,32 @@
+
+def PreviousClicked(event_struct):
+    return
+
+
+def NextClicked(event_struct):
+    return
+
+
+def BankAClicked(event_struct):
+    return
+
+
+def BankBClicked(event_struct):
+    return
+
+
+def AmpSkinNext(event_struct):
+    return
+
+
+def AmpSkinPrevious(event_struct):
+    return
+
+
+def PresetDescriptionChanged(event_struct):
+    return
+
+
+def SaveUserDataRequest(event_struct):
+    return
+
